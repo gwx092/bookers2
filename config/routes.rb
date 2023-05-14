f12b874046a12c1dc34995be5a1c36b41980e701
Kenji Senda <ec2-user@ip-172-31-29-7.ap-northeast-1.compute.internal>
2023-05-14 09:03:58 +0000

    [finish]4-27

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ devise_for :users, controllers: {
+    sessions: 'devise/sessions',
+    registrations: 'devise/registrations'
+  }
+
+  root "homes#top"
+  get "/home/about" => "homes#about"
+  resources :users
+  resources :books
 end
